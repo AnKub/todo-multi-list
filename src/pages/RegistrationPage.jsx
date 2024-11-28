afd7d64760df9bf7ffb1220710203b4689857247
AnKub <ankub3088@gmail.com>
2024-11-28 15:07:49 +0200

    clean code

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "../styles/RegistrationPage.scss"; // Использование модуля SCSS через импорт
+import styles from "../styles/RegistrationPage.scss" 
 
 const RegistrationPage = () => {
   const [formData, setFormData] = useState({
