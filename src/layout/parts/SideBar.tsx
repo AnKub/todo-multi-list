b70d6b1efd8d337d49e1c859653f03d9f29568b4
AnKub <ankub3088@gmail.com>
2025-01-01 17:30:41 +0200

    styles

--- src/layout/parts/SideBar.tsx
+++ src/layout/parts/SideBar.tsx
@@ -1,44 +1,44 @@
-// import React from "react";
-// import "../../styles/layoutStyle/sidebar.scss";
+import React from "react";
+import "../../styles/layoutStyle/sidebar.scss";
 
-// interface SidebarProps {
-//   isCollapsed: boolean;
-//   toggleSidebar: () => void;
-// }
+interface SidebarProps {
+  isCollapsed: boolean;
+  toggleSidebar: () => void;
+}
 
-// const Sidebar: React.FC<SidebarProps> = ({ isCollapsed, toggleSidebar }) => {
-//   return (
-//     <aside className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
-//       <button
-//         className={`toggle-button ${isCollapsed ? "icon-collapsed" : "icon-expanded"}`}
-//         onClick={toggleSidebar}
-//       >
-//         <span className="icon-wrapper">
-//           {isCollapsed ? (
-//             <img src="/img/left.svg" alt="Open Sidebar" className="icon-left" />
-//           ) : (
-//             <img src="/img/herre.svg" alt="Close Sidebar" className="icon-right" />
-//           )}
-//         </span>
-//       </button>
-//       {!isCollapsed && (
-//         <ul>
-//           <li>
-//             <a href="/todos">Todos</a>
-//           </li>
-//           <li>
-//             <a href="/calendar">Calendar</a>
-//           </li>
-//           <li>
-//             <a href="/blog">Blog</a>
-//           </li>
-//           <li>
-//             <a href="/articles">Articles</a>
-//           </li>
-//         </ul>
-//       )}
-//     </aside>
-//   );
-// };
+const Sidebar: React.FC<SidebarProps> = ({ isCollapsed, toggleSidebar }) => {
+  return (
+    <aside className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
+      <button
+        className={`toggle-button ${isCollapsed ? "icon-collapsed" : "icon-expanded"}`}
+        onClick={toggleSidebar}
+      >
+        <span className="icon-wrapper">
+          {isCollapsed ? (
+            <img src="/img/left.svg" alt="Open Sidebar" className="icon-left" />
+          ) : (
+            <img src="/img/herre.svg" alt="Close Sidebar" className="icon-right" />
+          )}
+        </span>
+      </button>
+      {!isCollapsed && (
+        <ul>
+          <li>
+            <a href="/todos">Todos</a>
+          </li>
+          <li>
+            <a href="/calendar">Calendar</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/articles">Articles</a>
+          </li>
+        </ul>
+      )}
+    </aside>
+  );
+};
 
-// export default Sidebar; 
+export default Sidebar; 
