2db429797a60a3aede0863232ebace9d0462e4f8
AnKub <ankub3088@gmail.com>
2025-01-03 11:07:16 +0200

    styles

--- src/layout/parts/Header.tsx
+++ src/layout/parts/Header.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"; 
 import "../../styles/layoutStyle/header.scss";
 
 const Header = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const navigate = useNavigate();
 
-  // Зміна теми
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
@@ -12,7 +13,11 @@ const Header = () => {
     document.documentElement.setAttribute("data-theme", newTheme);
   };
 
-  //теми
+  const handleLogout = () => {
+    localStorage.removeItem("authToken"); 
+    navigate("/"); 
+  };
+
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
@@ -20,15 +25,23 @@ const Header = () => {
   return (
     <header className="header">
       <h2>Multi-Todo</h2>
-      <button className={`theme-toggle ${theme === "light" ? "animate-light" : "animate-dark"}`} onClick={toggleTheme}>
-        <span className="icon-wrapper">
-          {theme === "light" ? (
-            <img src="/img/sun.svg" alt="Moon Icon" className="moon-icon" />
-          ) : (
-            <img src="/img/moon.svg" alt="Sun Icon" className="sun-icon" />
-          )}
-        </span>
-      </button>
+      <div className="header-actions">
+        <button
+          className={`theme-toggle ${theme === "light" ? "animate-light" : "animate-dark"}`}
+          onClick={toggleTheme}
+        >
+          <span className="icon-wrapper">
+            {theme === "light" ? (
+              <img src="/img/sun.svg" alt="Moon Icon" className="moon-icon" />
+            ) : (
+              <img src="/img/moon.svg" alt="Sun Icon" className="sun-icon" />
+            )}
+          </span>
+        </button>
+        <button className="logout-button" onClick={handleLogout}>
+        Out
+        </button>
+      </div>
     </header>
   );
 };
