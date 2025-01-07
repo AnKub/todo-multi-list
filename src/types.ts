4a9e04f6b6fcd4025c25485b05601cb9ea6efe41
AnKub <ankub3088@gmail.com>
2025-01-07 13:18:02 +0200

    styles

--- src/types.ts
+++ src/types.ts
@@ -1,49 +1,49 @@
-// types.ts
-export interface Task {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+
+// export interface Task {
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
 
 
-export interface TodoList {
-  id: number;
-  name: string;
-  tasks: Task[];
-}
+// export interface TodoList {
+//   id: number;
+//   name: string;
+//   tasks: Task[];
+// }
 
-export interface TaskItemProps {
-  task: Task;
-  onDelete: (taskId: number) => void;
-  onToggle: (taskId: number) => void;
-  onUpdate: (taskId: number, newTitle: string) => void;
-}
+// export interface TaskItemProps {
+//   task: Task;
+//   onDelete: (taskId: number) => void;
+//   onToggle: (taskId: number) => void;
+//   onUpdate: (taskId: number, newTitle: string) => void;
+// }
 
-export interface TodoGridProps {
-  todoLists: TodoList[];
-  onAddTodoList: (newListName: string) => void;
-  onDeleteTodoList: (listId: number) => void;
-  onOpenTodoList: (listId: number) => void;
-}
+// export interface TodoGridProps {
+//   todoLists: TodoList[];
+//   onAddTodoList: (newListName: string) => void;
+//   onDeleteTodoList: (listId: number) => void;
+//   onOpenTodoList: (listId: number) => void;
+// }
 
-export interface LoginFormData {
-  email: string;
-  password: string;
-}
+// export interface LoginFormData {
+//   email: string;
+//   password: string;
+// }
 
-export interface RegistrationFormData {
-  username: string;
-  email: string;
-  password: string;
-}
+// export interface RegistrationFormData {
+//   username: string;
+//   email: string;
+//   password: string;
+// }
 
-export interface LoginError {
-  email: string;
-  password: string;
-}
+// export interface LoginError {
+//   email: string;
+//   password: string;
+// }
 
-export interface RegistrationError {
-  username: string;
-  email: string;
-  password: string;
-}
+// export interface RegistrationError {
+//   username: string;
+//   email: string;
+//   password: string;
+// }
